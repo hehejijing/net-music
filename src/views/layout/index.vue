5944bc4a62a87ba197e24179f5190560eea1a60b
limu <1259567994@qq.com>
2022-07-24 21:26:03 +0800

    1

---
+++ src/views/layout/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+name: 'AA'
+}
+</script>
+
+<style>
+/* 中间内容区域 - 容器样式(留好上下导航所占位置) */
+.main {
+  padding-top: 46px;
+  padding-bottom: 50px;
+}
+</style>
